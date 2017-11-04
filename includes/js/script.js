cd2e61daeb7ebad00b1359f7135f48d2a7828c8f
Chingiz Mardanov <chingiz@li.ru>
2017-11-03 22:47:38 -0400

    Initial Commit

---
+++ includes/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	var itemToScroll = $('#firstBackground'); //First image
+	var secondItemToScroll = $('#secondBackground');
+	var currentPos = parseInt(itemToScroll.css('top'));
+	var secondCurrentPos = parseInt(secondItemToScroll.css('top'));
+			
+	$( window ).scroll(function() {	
+	  	var yPos = currentPos-($(window).scrollTop() / itemToScroll.data('speed'));
+	  	var yPosTwo = secondCurrentPos-($(window).scrollTop() / secondItemToScroll.data('speed'));
+	  	var coords = yPos + 'px';
+	    itemToScroll.css({ top: coords });
+	    var coordsTwo = yPosTwo + 'px';
+	    secondItemToScroll.css({ top: coordsTwo });
+
+	});
+});
+
+/*
+
+$(document).ready(function(){
+	var itemToScroll = $('#firstBackground'); //First image
+	
+	console.log(itemToScroll.data('speed'));
+	var currentPos = parseInt(itemToScroll.css('top'));
+			
+	$( window ).scroll(function() {	
+	  	var yPos = currentPos-($(window).scrollTop() / itemToScroll.data('speed'));
+	  	console.log(yPos);
+	  	var coords = yPos + 'px';
+	    itemToScroll.css({ top: coords });
+	});
+});
+*/
