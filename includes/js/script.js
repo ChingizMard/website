b53f1f8578a7285230461567a94f9b350af6a270
Chingiz Mardanov <chingiz@li.ru>
2017-11-14 10:28:45 -0500

    Improvements

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-	var itemToScroll = $('#firstBackground'); //First image
-	var secondItemToScroll = $('#secondBackground');
-	var currentPos = parseInt(itemToScroll.css('top'));
-	var secondCurrentPos = parseInt(secondItemToScroll.css('top'));
+// $(document).ready(function(){
+// 	var itemToScroll = $('#firstBackground'); //First image
+// 	var secondItemToScroll = $('#secondBackground');
+// 	var currentPos = parseInt(itemToScroll.css('top'));
+// 	var secondCurrentPos = parseInt(secondItemToScroll.css('top'));
 			
-	$( window ).scroll(function() {	
-	  	var yPos = currentPos-($(window).scrollTop() / itemToScroll.data('speed'));
-	  	var yPosTwo = secondCurrentPos-($(window).scrollTop() / secondItemToScroll.data('speed'));
-	  	var coords = yPos + 'px';
-	    itemToScroll.css({ top: coords });
-	    var coordsTwo = yPosTwo + 'px';
-	    secondItemToScroll.css({ top: coordsTwo });
+// 	$( window ).scroll(function() {	
+// 	  	var yPos = currentPos-($(window).scrollTop() / itemToScroll.data('speed'));
+// 	  	var yPosTwo = secondCurrentPos-($(window).scrollTop() / secondItemToScroll.data('speed'));
+// 	  	var coords = yPos + 'px';
+// 	    itemToScroll.css({ top: coords });
+// 	    var coordsTwo = yPosTwo + 'px';
+// 	    secondItemToScroll.css({ top: coordsTwo });
 
-	});
-});
+// 	});
+// });
 
 /*
 
